0625d1d72273f3b43f8c996cef2436833fdfe434
Nagy Levente <h049490@stud.u-szeged.hu>
2025-05-05 10:29:18 +0200

    Update frontend link

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {environment} from "../environments/environment";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'frontend';
+
+  ngOnInit() {
+    console.log('Current environment:', environment);
+    console.log('API URL:', environment.API_URL);
+  }
 }
